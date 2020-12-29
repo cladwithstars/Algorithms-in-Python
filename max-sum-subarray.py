9e87f613f31813a1d7307c22f37dc0c6a35092b3
Will Morgan <morgan88will@gmail.com>
2020-12-29 09:52:51 -0500

    kadanes algorithm

---
+++ max-sum-subarray.py
@@ -0,0 +1,12 @@
+#kadane's algorithm
+
+def maxSubArray(nums):
+        maxSum = nums[0]
+        currentSum = nums[0]
+        for i in range(1,len(nums)):
+            print(f'currSum: {currentSum}, maxSum: {maxSum}')
+            currentSum = max(nums[i] + currentSum, nums[i])
+            maxSum = max(maxSum, currentSum)
+        return maxSum
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
