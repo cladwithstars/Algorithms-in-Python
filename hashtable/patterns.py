5598a2de0cafd93ded246c629711812440844444
Will Morgan <morgan88will@gmail.com>
2022-01-06 22:20:26 -0500

    areFollowingPatterns

---
+++ hashtable/patterns.py
@@ -0,0 +1,33 @@
+def solution(strings, patterns):
+    """
+    Given an array strings, determine whether it follows the sequence given in the patterns array.
+    In other words, there should be no i and j for which strings[i] = strings[j]
+    and patterns[i] ≠ patterns[j] or for which strings[i] ≠ strings[j] and 
+    patterns[i] = patterns[j]
+    """
+    d = dict()
+    seen = set()
+    for i in range(len(patterns)):
+        
+        if patterns[i] in d:
+            if d[patterns[i]] != strings[i]:
+                return False
+            seen.add(strings[i])
+        else:
+            if strings[i] in seen:
+                return False
+            seen.add(strings[i])
+            d[patterns[i]] = strings[i]
+            
+    return True
+
+strings = ["cat", "dog", "doggy"]
+patterns = ["a", "b", "b"]
+
+print(solution(strings, patterns)) #expect false
+
+strings = ["cat", "dog", "dog"]
+patterns = ["a", "b", "b"]
+
+print(solution(strings, patterns)) #expect true
+
