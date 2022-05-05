9e193cd8d0588a3d3c5087b23f8778aefeed382e
William Morgan <william_morgan@flutterint.com>
2022-05-05 15:09:33 -0400

    .

--- updateMatrix.py
+++ updateMatrix.py
@@ -1,5 +1,5 @@
 def updateMatrix(mat):
-    res = [[-1 for _ in range(len(mat[0]))] for _ in range(len(mat))]
+    res = [[0 for _ in range(len(mat[0]))] for _ in range(len(mat))]
 
     def getAdjacentCells(i, j):
         candidates = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
@@ -8,23 +8,29 @@ def updateMatrix(mat):
             if cell[0] >= 0 and cell[0] < len(mat) and cell[1] >= 0 and cell[1] < len(mat[0]):
                 res.append(cell)
         return res
-    visited = set()
 
-    def getDistance(i, j):
-        if mat[i][j] == 0:
-            return 0
-        visited.add((i, j))
-        adjCells = getAdjacentCells(i, j)
-        adjDistances = []
-        for cell in adjCells:
-            if cell not in visited:
-                adjDistances.append(getDistance(cell[0], cell[1]))
-        visited.remove((i, j))
-        return 1 + min(adjDistances)
+    def bfs(cell):
+        from collections import deque
+        q = deque()
+        i, j = cell
+        q.append(((i, j), 0))
+        visited = set()
+        while q:
+            for i in range(len(q)):
+                cell, d = q.popleft()
+                x, y = cell
+                if mat[x][y] == 0:
+                    return d
+                visited.add(cell)
+                for adjCell in getAdjacentCells(x, y):
+                    if adjCell not in visited:
+                        q.append(((adjCell[0], adjCell[1]), d + 1))
+        return -1
 
     for i in range(len(mat)):
         for j in range(len(mat[0])):
-            res[i][j] = getDistance(i, j)
+            if mat[i][j] == 1:
+                res[i][j] = bfs((i, j))
     return res
 
 
